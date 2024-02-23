ed98edbcd17c5d0011d30accf0437e23abbdd7e4
chandru <chandrukongari@gmail.com>
2024-02-23 19:17:41 +0530

    newsgemini v1

--- NewsGemini/main.py
+++ NewsGemini/main.py
@@ -1,20 +1,32 @@
-import os
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv() # loading all the envrionmant variable
 import google.generativeai as genai
 
 
-genai.configure(api_key="Google_api_key")
+genai.configure(api_key="Your_Google_API_Key")
 
 
-def get_gemini_response(input_text, pdf_content, prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input_text, pdf_content[0], prompt])
+def generate_newspaper_article(full_article):
+  
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(full_article)
     return response.text
-## Streamlit App
 
-st.set_page_config(page_title="NewsGemini")
-st.header("NewsGemini")
+# Streamlit app
+def main():
+    st.title("NewsGemini: Newspaper Article Generator")
+    st.markdown("---")
+    
 
-input_text = st.text_area("Artical: ", key="input")
+    full_article = st.text_area("Input Full Article", height=200)
+
+    if st.button("Generate Newspaper Article"):
+        if full_article:
+           
+            newspaper_article = generate_newspaper_article(full_article)
+            st.subheader("Generated Newspaper Article:")
+            st.write(newspaper_article)
+        else:
+            st.warning("Please input a full article.")
+
+if __name__ == "__main__":
+    main()
